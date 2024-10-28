2f43f266a3ab75f2aa641ecd212c8ee6fe137320
ordazkim <martinezordaz.kimberly@gmail.com>
2024-10-27 22:18:15 -0400

    Completed Lab 1-4 and started lab 1-5

--- student_Xcode15/Unit1/Lesson1_4/Lab_Operators.playground/Pages/1. Exercise - Basic Arithmetic.xcplaygroundpage/Contents.swift
+++ student_Xcode15/Unit1/Lesson1_4/Lab_Operators.playground/Pages/1. Exercise - Basic Arithmetic.xcplaygroundpage/Contents.swift
@@ -1,21 +1,26 @@
 /*:
-## Exercise - Basic Arithmetic
- 
- You decide to build a shed and want to know beforehand the area of your yard that it will take up. Create two constants, `width` and `height`, with values of 10 and 20, respectively. Create an `area` constant that is the result of multiplying the two previous constants together, and print out the result.
- */
-
+ ## Exercise - Basic Arithmetic
+  
+  You decide to build a shed and want to know beforehand the area of your yard that it will take up. Create two constants, `width` and `height`, with values of 10 and 20, respectively. Create an `area` constant that is the result of multiplying the two previous constants together, and print out the result.
+  */
+var width = 10
+var height = 20
+var area = width * height
+print(area)
 
 //:  You decide that you'll divide your shed into two rooms. You want to know if dividing it equally will leave enough room for some of your larger storage items. Create a `roomArea` constant that is the result of dividing `area` in half. Print out the result.
-
-
-//:  Create a `perimeter` constant whose value equals `width` plus `width` plus `height` plus `height`, then print out the result.
-
-
+var roomArea = area/2
+print(roomArea)
 //:  Print what you would expect the result of integer division of 10 divided by 3 to be. Create a constant, `integerDivisionResult` that is the result of 10 divided by 3, and print the value.
-
-
+print(3.3333)
+var intergerDivisionResult = 10/3
+print(intergerDivisionResult)
 //:  Now create two constants, `double10` and `double3`, set to 10 and 3, and declare their types as `Double` values. Declare a final constant `divisionResult` equal to the result of `double10` divided by `double3`. Print the value of `divisionResult`. How does this differ from the value when using integer division?
-
+let x:Double = 10
+let y:Double = 3
+let divisionResult = x / y
+print(divisionResult)
+print("It diffeers by showing th decimals the diviision has")
 
 /*:
  Given the value pi (3.1415927), create a `radius` constant with a value of 5.0, then calculate the diameter and circumference of the circle using the following equations, and print the results:
