954e96346cff05e6e8211dcd603728f885e5ddaf
ordazkim <martinezordaz.kimberly@gmail.com>
2024-11-20 18:12:43 -0500

    Lesson 2-3 Completed

--- student_Xcode15/Unit2/Lesson2_3/Lab_Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
+++ student_Xcode15/Unit2/Lesson2_3/Lab_Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
@@ -3,12 +3,30 @@
 
  Write a function called `introduceMyself` that prints a brief introduction of yourself. Call the function and observe the printout.
  */
-
-
+func introduceMyself() {
+    print("Hello, my name is Kimberly M. and I like to cook.")
+}
+introduceMyself()
 //:  Write a function called `magicEightBall` that generates a random number and then uses either a switch statement or if-else-if statements to print different responses based on the random number generated. `let randomNum = Int.random(in: 0...4)` will generate a random number from 0 to 4, after which you can print different phrases corresponding to the number generated. Call the function multiple times and observe the different printouts.
 import Foundation
-
-
+func magicEightBall(){
+    let randomNum = Int.random(in: 0...4)
+    switch randomNum{
+    case 0:
+        print("It is certain")
+    case 1:
+        print("Ask Again")
+    case 2:
+        print("Yes")
+    case 3:
+        print("No")
+    case 4:
+        print("Maybe")
+    default:
+        print("Error: Unexpected number")
+    }
+}
+magicEightBall()
 /*:
 page 1 of 6  |  [Next: App Exercise - A Functioning App](@next)
  */
--- student_Xcode15/Unit2/Lesson2_3/Lab_Functions.playground/Pages/5. Exercise - Return Values.xcplaygroundpage/Contents.swift
+++ student_Xcode15/Unit2/Lesson2_3/Lab_Functions.playground/Pages/5. Exercise - Return Values.xcplaygroundpage/Contents.swift
@@ -3,11 +3,17 @@
 
  Write a function called `greeting` that takes a `String` argument called name, and returns a `String` that greets the name that was passed into the function. I.e. if you pass in "Sophie" the return value might be "Hi, Sophie! How are you?" Use the function and print the result.
  */
-
-
+func greeting(name: String) -> String{
+    return "Hello, \(name)! Nice to meet you."
+}
+let greetMessage = greeting(name: "Kim")
+print(greetMessage)
 //:  Write a function that takes two `Int` arguments, and returns an `Int`. The function should multiply the two arguments, add 2, then return the result. Use the function and print the result.
-
-
+func multiplyAndAddTwo(num1: Int, num2: Int) -> Int{
+    return (num1 * num2) + 2
+}
+let result = multiplyAndAddTwo(num1: 2, num2: 4)
+print("The result is \(result)")
 /*:
 [Previous](@previous)  |  page 5 of 6  |  [Next: App Exercise - Separating Functions](@next)
  */
